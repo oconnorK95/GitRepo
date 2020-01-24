9f34da00a174782668cb0db2d83ff30161178487
oconnorK95 <55146318+oconnorK95@users.noreply.github.com>
2020-01-24 13:59:11 +0000

    First Commmit

---
+++ OConnor_K_PracticalGaming_2020/Assets/Scripts/Ship.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+   //This script contains a definition of a spaceship
+   //A spaceship is made up of a number of ship components
+   //Select a random member of each component array to populate the appropriate empty gameobjects on the ship
+   //Wings should be placed into the wings empty gameobject, thrusters into the thrust gameobject etc
+
+
+    //C# does not allow multiple inheritance...
+    //Merge all the abstract classes into 1 or use interfaces?
+    void Start()
+    {
+        
+    }
+
+  
+    void Update()
+    {
+        
+    }
+}
---
+++ OConnor_K_PracticalGaming_2020/Assets/Scripts/ShipComponent.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ShipComponent : MonoBehaviour
+{
+    //This class contains a template for the components which make up a spaceship
+    //Essential: Hull, Side + Rear Thrusters, Wings
+    //NonEssential: WarpDrive, Hyperdrive, Accents, Guns
+    //These components will have values associated with them which will add to the ships total stats. E.g. a hull will add health, thrusters add speed
+
+
+    private int shipMaxValue;
+    private int shipMaxHealth;
+    private int shipCurValue;
+    private int shipCurHealth;
+    private int shipSpeed;
+    private int shipTurnSpeed;
+    private int shipDamage;
+
+    private GameObject[] wings;
+    //hulls.Add(wings1);
+    //hulls.Add(wings2);
+    //hulls.Add(wings3);
+
+    private GameObject[] guns;
+    //hulls.Add(guns1);
+    //hulls.Add(guns2);
+    //hulls.Add(guns3);
+
+
+    private GameObject[] hulls;
+    //hulls.Add(hull1);
+    //hulls.Add(hull2);
+    //hulls.Add(hull3);
+
+    private GameObject[] rearThruster;
+    //hulls.Add(rearThruster1);
+    //hulls.Add(rearThruster2);
+    //hulls.Add(rearThruster3);
+
+    private GameObject[] thrusters;
+    //hulls.Add(thrusters1);
+    //hulls.Add(thrusters2);
+    //hulls.Add(thrusters3);
+
+}
