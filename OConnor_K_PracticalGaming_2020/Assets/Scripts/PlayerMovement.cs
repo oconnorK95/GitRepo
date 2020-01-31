20db265db1dcd5f12c73dcf972e67d88654502ee
oconnorK95 <55146318+oconnorK95@users.noreply.github.com>
2020-01-31 10:45:46 +0000

    Set up spaceship game-objects

--- OConnor_K_PracticalGaming_2020/Assets/Scripts/PlayerMovement.cs
+++ OConnor_K_PracticalGaming_2020/Assets/Scripts/PlayerMovement.cs
@@ -1,66 +1,40 @@
 ﻿using System.Collections;
-
 using System.Collections.Generic;
-
 using UnityEngine;
 
 
 
 public class PlayerMovement : MonoBehaviour
-
 {
 
     [SerializeField]
-
     private Rigidbody rb;
-
-    private bool grounded = true;
-
+    //private bool grounded = true;
     private int speed = 15;
 
-
-
     public CharacterController controller;
-
-
 
     // Start is called before the first frame update
 
     void Start()
-
     {
-
         rb = GetComponent<Rigidbody>();
-
     }
 
-
-
     // Update is called once per frame
-
     void Update()
-
     {
-        rb.AddForce(Vector3.down * Time.deltaTime, ForceMode.Acceleration);
+        //rb.AddForce(Vector3.down * Time.deltaTime, ForceMode.Acceleration);
+        //rb.transform.Translate(Vector3.down * Time.deltaTime);
+
         Vector3 move = transform.right * Input.GetAxis("Horizontal") + transform.forward * Input.GetAxis("Vertical");
 
-
-
         //rb.AddForce(move * Time.deltaTime);
-
         controller.Move(move * speed * Time.deltaTime);
-
-
 
         if (Input.GetKey(KeyCode.Space))
         {
-
             rb.AddForce(Vector3.up, ForceMode.Impulse);
-
         }
-
-
-
     }
-
 }
--- OConnor_K_PracticalGaming_2020/Assets/Scripts/ShipComponent.cs
+++ OConnor_K_PracticalGaming_2020/Assets/Scripts/ShipComponent.cs
@@ -15,32 +15,18 @@ private int shipSpeed;
 private int shipTurnSpeed;
 private int shipDamage;
 */
-    [SerializeField]GameObject wings1, wings2, wings3;
-    [SerializeField]GameObject guns1, guns2, guns3;
-    [SerializeField]GameObject hulls1, hulls2, hulls3;
+    [SerializeField] GameObject wings1, wings2, wings3, wings4, wings5;
+    //[SerializeField]GameObject guns1, guns2, guns3;
+    [SerializeField]GameObject hulls1, hulls2, hulls3, hulls4, hulls5;
     [SerializeField]GameObject thrusters1, thrusters2, thrusters3;
 
+    /*
     private GameObject[] wings;
-    //public GameObject wings.Add(wings1);
-    //public GameObject = wings[0];
-    //wings.Add(wings2);
-    //wings.Add(wings3);
-
     private GameObject[] guns;
-    //guns.Add(guns1);
-    //guns.Add(guns2);
-    //guns.Add(guns3);
-
     private GameObject[] hulls;
-    //hulls.Add(hull1);
-    //hulls.Add(hull2);
-    //hulls.Add(hull3);
-
     private GameObject[] thrusters;
-    //thrusters.Add(thrusters1);
-    //thrusters.Add(thrusters2);
-    //thrusters.Add(thrusters3);
-
+    */
+   
     //Select random element from each array, wings, guns, hulls, thrusters
     //Add each component to ship
     //Accessors mutators to update ship components, find parts in world. Use arc welder to add part (show opaque outline of component to be added)
